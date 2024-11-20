00cf5d040d379e11b03f3ff49618a92c29ef4b67
sandro.vieira <sandro.vieira@ambevtech.com.br>
2024-11-20 12:51:28 -0300

    Creating a temporary database

---
+++ Movies.Application/Repositories/IMovieRepository.cs
@@ -0,0 +1,13 @@
+﻿using Movies.Application.Model;
+
+namespace Movies.Application.Repositories
+{
+    public interface IMovieRepository
+    {
+        Task<bool> CreateAsync(Movie movie);
+        Task<Movie?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Movie>> GetAllAsync();
+        Task<bool> UpdateAsync(Movie movie);
+        Task<bool> DeleteByIdAsync(Guid id);
+    }
+}
