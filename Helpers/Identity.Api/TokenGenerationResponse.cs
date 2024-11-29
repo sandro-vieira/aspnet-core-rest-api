9377aaf44daa9d1b9c773a5d61936f0e786b1d9e
sandro.vieira <sandro.vieira@ambevtech.com.br>
2024-11-29 18:39:40 -0300

    New project Identity.Api

---
+++ Helpers/Identity.Api/TokenGenerationResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Identity.Api
+{
+    public class TokenGenerationResponse
+    {
+        [JsonPropertyName("token_type")]
+        public required string TokenType { get; set; }
+        [JsonPropertyName("expires_in")]
+        public DateTime? ExpiresIn { get; set; }
+        [JsonPropertyName("access_token")]
+        public required string AccessToken { get; set; }
+    }
+}
