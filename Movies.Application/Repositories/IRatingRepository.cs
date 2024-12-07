5ea7b13ae5ee681234f29117449a241160e32cde
sandro.vieira <sandro.vieira@ambevtech.com.br>
2024-12-07 10:35:31 -0300

    Deleting a rating

--- Movies.Application/Repositories/IRatingRepository.cs
+++ Movies.Application/Repositories/IRatingRepository.cs
@@ -5,5 +5,6 @@ public interface IRatingRepository
     Task<float?> GetRatingAsync(Guid movieId, CancellationToken token = default);
     Task<(float? Rating, int? UserRating)> GetRatingAsync(Guid movieId, Guid userId, CancellationToken token = default);
     Task<bool> RateMovieAsync(Guid movieId, int rating, Guid userId, CancellationToken token = default);
+    Task<bool> DeleteRatingAsync(Guid movieId, Guid userId, CancellationToken token = default);
 }
 
